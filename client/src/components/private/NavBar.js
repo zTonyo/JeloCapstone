df5c37510a917f01500d44991f814140c690558b
zTonyo <christ.tonio.09@gmail.com>
2025-01-18 02:31:25 +0800

    last na

--- client/src/components/private/NavBar.js
+++ client/src/components/private/NavBar.js
@@ -2,19 +2,15 @@ import React, { useEffect } from 'react';
 
 const NavBar = () => {
   return (
-    <div className='vertical light'>
-      <nav className='topnav navBar navbar-light'>
-        <button type='button' className='navbar-toggler text-muted mt-2 p-0 mr-3 collapseSidebar'>
-          <i className='fe fe-menu navbar-toggler-icon'></i>
+    <div className=''>
+      <nav className='navBar navbar-light'>
+        <button type='button' className='navbar-toggler'>
+          <i className='navbar-toggler-icon'></i>
         </button>
-        <span className='avatar avatar-sm'>
-          <div className='avatar-img rounded-circle avatar-initials-min text-center position-relative'></div>
+        <span className='avatar'>
+          <div className='avatar-img avatar-initials-min'></div>
         </span>
       </nav>
-        <aside class="sidebar-left border-right bg-white" id="leftSidebar" data-simplebar></aside>
-        <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
-          <i class="fe fe-x"><span class="sr-only"></span></i>
-        </a>
     </div>
   );
 }
