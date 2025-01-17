4c62907a27ef83cc7874b7e88a2b86fecc8894b9
zTonyo <christ.tonio.09@gmail.com>
2025-01-18 02:34:49 +0800

    30%

--- client/src/components/private/NavBar.js
+++ client/src/components/private/NavBar.js
@@ -1,16 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 
 const NavBar = () => {
+  const [isOffcanvasVisible, setOffcanvasVisible] = useState(false);
+  
+  const toggleOffcanvas = () => {
+    setOffcanvasVisible(!isOffcanvasVisible);
+  };
+
   return (
-    <div className=''>
-      <nav className='navBar navbar-light'>
-        <button type='button' className='navbar-toggler'>
-          <i className='navbar-toggler-icon'></i>
+    <div>
+      <nav className="navBar navbar-light">
+        <button type="button" className="navbar-toggler" onClick={toggleOffcanvas}>
+          <i className="navbar-toggler-icon"></i>
         </button>
-        <span className='avatar'>
-          <div className='avatar-img avatar-initials-min'></div>
+        <span className="avatar">
+          <div className="avatar-img avatar-initials-min"></div>
         </span>
       </nav>
+
+      {/* Offcanvas Sidebar */}
+      <div className={`offcanvas offcanvas-start ${isOffcanvasVisible ? 'show' : ''}`} tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasExampleLabel">Sidebar</h5>
+          <button type="button" className="btn-close text-reset" onClick={toggleOffcanvas} aria-label="Close"></button>
+        </div>
+        <div className="offcanvas-body">
+          <p>Some content for the sidebar.</p>
+          <p>More content here.</p>
+        </div>
+      </div>
     </div>
   );
 }
