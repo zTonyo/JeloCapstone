cd5c3223b1d05aefc54327ef4ecad50251841050
zTonyo <christ.tonio.09@gmail.com>
2025-01-18 00:26:14 +0800

    mema update

--- client/src/components/private/NavBar.js
+++ client/src/components/private/NavBar.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+
 
 const NavBar = ({ onLogout }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          My Website
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
+    <nav class="navbar nav-sidebar navbar-light mb-3">
+    <div class="container-fluid d-flex justify-content-between">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="offcanvas offcanvas-sidebar offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header justify-content-between">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+          <button type="button" class="navbar-toggler navbar-toggler-icon" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link" href="#action1">Home</a>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/teacherlogin">
-                Teacher Dashboard
-              </Link>
+            <li class="nav-item">
+              <a class="nav-link" href="#action2">Link</a>
             </li>
-            <li className="nav-item">
-              <button className="btn btn-link nav-link" onClick={onLogout}>
-                Logout
-              </button>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#action3">Action</a></li>
+                <li><a class="dropdown-item" href="#action4">Another action</a></li>
+                <li><hr class="dropdown-divider"/></li>
+                <li><a class="dropdown-item" href="#action5">Something else here</a></li>
+              </ul>
             </li>
           </ul>
+          <form class="d-flex ms-auto">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
