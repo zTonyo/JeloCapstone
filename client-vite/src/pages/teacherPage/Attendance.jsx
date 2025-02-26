420f467ffa5d26cdae96ae261e97f01a14c2ee91
Lanxxxe <Lanxxxe>
2025-02-27 01:39:42 +0800

    create: teacher dashboard template create: teacher teachermanagement template create: teacher attendance template

--- client-vite/src/pages/teacherPage/Attendance.jsx
+++ client-vite/src/pages/teacherPage/Attendance.jsx
@@ -1,7 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Table, Form } from "react-bootstrap";
 
 function Attendance({ sidebarOpen }) {
+
+  const [teachers, setTeachers] = useState([
+      {
+        position: "Teacher",
+        name: "John Doe",
+        contact: "09123456789",
+        schedule: "K1- 8:00am-10:00am",
+        email: "john@example.com",
+      },
+      {
+        position: "Admin",
+        name: "Jane Smith",
+        contact: "09876543210",
+        schedule: "K2- 10:15am-12:15nn",
+        email: "jane@example.com",
+      },
+    ]);
+  
+    const handleChange = (index, field, value) => {
+      const updatedTeachers = [...teachers];
+      updatedTeachers[index][field] = value;
+      setTeachers(updatedTeachers);
+    };
   const navigate = useNavigate();
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -14,8 +38,31 @@ function Attendance({ sidebarOpen }) {
   return (
     <div>
       <div className={`admin-body ${sidebarOpen ? 'without-sidebar' : 'with-sidebar'}`}>
-        <h2>Attendance</h2>
-        <p>Welcome to your Attendance</p>
+      <h2>Teacher Management</h2>
+        <div className='container-fluid px-3 mt-3'>
+          <Table striped bordered hover responsive>
+            <thead className="table-primary">
+              <tr>
+                <th>Student ID</th>
+                <th>Student Full Name</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  ID
+                </td>
+                <td>
+                  Student Name                  
+                </td>
+                <td>
+                  Present/Absent
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
   );
